b4493693194973b955a115ee23e4dbe06cddca80
Karl Young <Karlyoung112@gmail.com>
2024-03-05 15:47:31 +1300

    added preloader working on positions

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -13,6 +13,7 @@ function App() {
     setShowCanvas(!showCanvas)
   }
 
+  
   return (
     <>
       <div>
--- client/components/Description.tsx
+++ client/components/Description.tsx
@@ -1,34 +1,37 @@
 const Description = () => {
   return (
-    <section className="h-screen px-20 flex justify-center items-center gap-3 py-52">
-      <div className="w-80">
-        <img
-          className="rounded-2xl w-full h-auto"
-          src="client/styles/images/ME 2023.jpg"
-          alt="Karl Young"
-          id="me"
-        />
-      </div>
-      <article
-        id="about"
-        className="p-8 max-w-screen-md bg-slate-600 rounded-2xl"
-      >
-        <div>
-          <h1>Description</h1>
-          <p>
-            Something about my experience as a web dev when shrunk all this
-            moves to the right on top of a picture or something
-          </p>
-          <p>
-            Something about my experience as a web dev when shrunk all this
-            moves to the right on top of a picture or something
-          </p>
-          <p>
-            Something about my experience as a web dev when shrunk all this
-            moves to the right on top of a picture or something
-          </p>
+    <section className="relative h-screen px-10 flex justify-center items-center gap-3 py-52">
+      <div id="image-description-container">
+        <div className="w-80 absolute left-20  z-20">
+          <img
+            className="rounded-full w-full h-auto lg:rounded-2xl md:rounded-xl sm:rounded-full"
+            src="client/styles/images/ME 2023.jpg"
+            alt="Karl Young"
+            id="me"
+          />
         </div>
-      </article>
+        <article
+          id="about"
+          className="p-8 max-w-screen-md bg-slate-600 rounded-2xl relative z-10
+        lg:p-16 md:p-12 sm:p-8"
+        >
+          <div>
+            <h1>Description</h1>/
+            <p>
+              Something about my experience as a web dev when shrunk all this
+              moves to the right on top of a picture or something
+            </p>
+            <p>
+              Something about my experience as a web dev when shrunk all this
+              moves to the right on top of a picture or something
+            </p>
+            <p>
+              Something about my experience as a web dev when shrunk all this
+              moves to the right on top of a picture or something
+            </p>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }

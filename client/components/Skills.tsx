2e9510e912c9e1bbea0c3569ba2d0249b13a3a7d
Karl Young <Karlyoung112@gmail.com>
2024-05-21 17:47:53 +1200

    Added skills svg

--- client/components/Skills.tsx
+++ client/components/Skills.tsx
@@ -1,98 +1,103 @@
+const skills = [
+  {
+    name: 'HTML',
+    src: 'client/styles/images/html-5-svgrepo-com.svg',
+    alt: 'HTML logo',
+  },
+  {
+    name: 'CSS3',
+    src: 'client/styles/images/css-3-svgrepo-com.svg',
+    alt: 'CSS3 logo',
+  },
+  {
+    name: 'JavaScript',
+    src: 'client/styles/images/javascript-svgrepo-com.svg',
+    alt: 'JavaScript logo',
+  },
+  {
+    name: 'React',
+    src: 'client/styles/images/react-svgrepo-com.svg',
+    alt: 'React logo',
+  },
+  {
+    name: 'Git',
+    src: 'client/styles/images/git-svgrepo-com.svg',
+    alt: 'Git logo',
+  },
+  {
+    name: 'GitHub',
+    src: 'client/styles/images/github-142-svgrepo-com.svg',
+    alt: 'GitHub logo',
+  },
+  {
+    name: 'NPM',
+    src: 'client/styles/images/npm-svgrepo-com.svg',
+    alt: 'NPM logo',
+  },
+  {
+    name: 'Node.js',
+    src: 'client/styles/images/node-js-svgrepo-com.svg',
+    alt: 'Node.js logo',
+  },
+  {
+    name: 'TypeScript',
+    src: 'client/styles/images/typescript-svgrepo-com.svg',
+    alt: 'TypeScript logo',
+  },
+  {
+    name: 'TailWind',
+    src: 'client/styles/images/tailwind-svgrepo-com.svg',
+    alt: 'TailWind logo',
+  },
+  {
+    name: 'Vite',
+    src: 'client/styles/images/vite-svgrepo-com.svg',
+    alt: 'Vite logo',
+  },
+  {
+    name: 'VS Code',
+    src: 'client/styles/images/vscode-svgrepo-com.svg',
+    alt: 'VS Code logo',
+  },
+  {
+    name: 'SQLite',
+    src: 'client/styles/images/sqlite-svgrepo-com.svg',
+    alt: 'SQLite logo',
+  },
+  {
+    name: 'Express',
+    src: 'client/styles/images/express-svgrepo-com.svg',
+    alt: 'Express logo',
+  },
+  {
+    name: 'Knex',
+    src: 'client/styles/images/knex-svgrepo-com.svg',
+    alt: 'Knex logo',
+  },
+  {
+    name: 'Python',
+    src: 'client/styles/images/python-svgrepo-com.svg',
+    alt: 'Python logo',
+  },
+]
+
 const Skills = () => {
   return (
     <>
       <div>
         <h1>My Skills</h1>
-        <div>
-          <a href="https://react.dev/">
+      </div>
+      <div className="bg-natural flex flex-row flex-wrap justify-center">
+        {skills.map((skill, index) => (
+          <div key={index} className="flex flex-col items-center m-4">
             <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/63254261-4fe6-4ce5-b289-047d60f9a65f"
-              alt="React logo"
-              width="50"
-              height="50"
+              src={skill.src}
+              alt={skill.alt}
+              className="w-20 h-20 border-slate-700 bg-neutral rounded-full"
             />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.w3schools.com/js/default.asp">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/5bcdefa6-bb55-4f93-837a-5d17f69a5e68"
-              alt="JS logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.w3schools.com/css/">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/77277b6a-c6d8-4c1a-830f-9c841f02d746"
-              alt="CSS logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.w3schools.com/html/default.asp">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/62501f40-bc69-4700-9636-6296665c40b7"
-              alt="html logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://nodejs.org/en/about">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/95a142ca-6bca-4663-af52-57d46bf99815"
-              alt="Nodejs logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://expressjs.com/">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/2f2137b7-c392-4974-9d6f-9f69ca48b1c1"
-              alt="expressjs logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.typescriptlang.org/">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/c348f9da-e6b6-4adb-8dc0-097c395a9271"
-              alt="TS logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://git-scm.com/">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/1885740c-01cd-4d35-a555-2249306e92e9"
-              alt="Git logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="https://tailwindcss.com/">
-            <img
-              src="https://github.com/karl-young/karl-young/assets/142771183/121b77c3-1c82-4a38-86bd-ecea2a01bba4"
-              alt="Tailwind logo"
-              width="50"
-              height="50"
-            />
-          </a>
-        </div>
+            <h3>{skill.name}</h3>
+          </div>
+        ))}
       </div>
     </>
   )
